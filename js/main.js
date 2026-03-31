7690eafc9063f2ac20d6d56d15fcbf4c43b3cfab
king-watson <watsonkingsley38@gmail.com>
2026-03-31 03:10:01 -0400

    nav bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import './modules/hamburger.js';
-import './modules/animations.js';
-import { Counter, StepCounter } from './modules/counter.js';
 
 const navLinks = document.querySelectorAll("#menu a, #desktopMenu a");
 
@@ -17,8 +15,3 @@ function handleNavClick(e) {
 navLinks.forEach(function addScrollListener(link) {
     link.addEventListener("click", handleNavClick);
 });
-
-const counter1 = new Counter("#counter-container1", 10);
-const counter2 = new Counter("#counter-container2");
-const counter3 = new Counter("#counter-container3");
-const counter4 = new StepCounter("#counter-container4", 10, 5);
